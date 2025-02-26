daf3bc451591a0a95e48c7e145b53f89c88033cc
AidarusIkarus <muginov.aidar@gmail.com>
2025-02-26 22:45:46 +0300

    задача 3 Шахматная клетка

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 import pygame
 
 
-def rectangle():
+def chess():
     screen.fill((0, 0, 0))
-    pygame.draw.rect(screen, (255, 0, 0), (1, 1, width - 1, height - 1))
+    cell = size // num
+    colors = [(255, 255, 255), (0, 0, 0)]
+    for x in range(num):
+        for y in range(num):
+            color = colors[(x + y) % 2]
+            pygame.draw.rect(screen, color, (x * cell, y * cell, cell, cell))
 
 
 if __name__ == '__main__':
     pygame.init()
-    pygame.display.set_caption('Прямоугольник')
+    pygame.display.set_caption('Шахматная клетка')
 
     try:
-        x, y = map(int, input().split())
+        size, num = map(int, input().split())
     except ValueError:
         raise ValueError('Неправильный формат ввода')
 
-    width, height = x, y
-    screen = pygame.display.set_mode((width, height))
-    rectangle()
+    screen = pygame.display.set_mode((size, size))
+    chess()
     pygame.display.flip()
 
     while pygame.event.wait().type != pygame.QUIT:
