e0d410dbdb3983500966d5b86e6ec16358b7784b
AidarusIkarus <muginov.aidar@gmail.com>
2025-02-26 23:26:23 +0300

    задача 5 Сфера

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
 import pygame
 
 
-def chess():
+def sphere():
     screen.fill((0, 0, 0))
-    colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
-
-    for i in range(1, num + 1):
-        color = colors[(i - 1) % 3]
-        pygame.draw.circle(screen, color, (num * width, num * width), i * width, width)
+    step = 150 // num
+    for i in range(0, num):
+        pygame.draw.ellipse(screen, (255, 255, 255), (i * step, 0, 300 - i * 2 * step, 300), 1)
+        pygame.draw.ellipse(screen, (255, 255, 255), (0, i * step, 300, 300 - i * 2 * step), 1)
 
 
 if __name__ == '__main__':
     pygame.init()
-    pygame.display.set_caption('Мишень')
+    pygame.display.set_caption('Сфера')
 
     try:
-        width, num = map(int, input().split())
+        num = int(input())
     except ValueError:
         raise ValueError('Неправильный формат ввода')
 
-    size = (2 * num * width, 2 * num * width)
+    size = (300, 300)
     screen = pygame.display.set_mode(size)
-    chess()
+    sphere()
     pygame.display.flip()
 
     while pygame.event.wait().type != pygame.QUIT:
