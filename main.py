873c2f28a70cbbcd526c19e6798a618ea00e4422
AidarusIkarus <muginov.aidar@gmail.com>
2025-03-01 01:06:26 +0300

    pg2 2 Шарики

--- main.py
+++ main.py
@@ -1,36 +1,51 @@
 import pygame
 import os
 
-radius = float(0)
-
-
-def circle():
-    global radius
-    screen.fill(pygame.Color('blue'))
-    if boolean:
-        pygame.draw.circle(screen, 'yellow', (x, y), int(radius))
-        radius += 10 * clock.tick() / 1000
-
-
 if __name__ == '__main__':
     pygame.init()
-    pygame.display.set_caption('Жёлтый круг')
+    pygame.display.set_caption('Шарики')
     size = (300, 300)
 
     screen = pygame.display.set_mode(size)
     clock = pygame.time.Clock()
     running = True
     boolean = False
+    radius = 10
+    circles = []
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
-                radius = float(0)
                 x, y = event.pos
-                boolean = True
+                if x < radius:
+                    x = radius
+                if y < radius:
+                    y = radius
+                circles.append([x, y, 1, -1])
 
-        circle()
+        screen.fill('black')
+        dt = clock.tick() / 1000
+        for elem in circles:
+            cur_x, cur_y, x_dir, y_dir = elem
+            pygame.draw.circle(screen, 'white', (int(cur_x), int(cur_y)), radius)
+            elem[0] -= x_dir * 100 / 1.414 * dt
+            elem[1] += y_dir * 100 / 1.414 * dt
+            if cur_x < radius:
+                elem[0] = radius
+                elem[2] = -elem[2]
+
+            if cur_x > size[0] - radius:
+                elem[0] = size[0] - radius
+                elem[2] = -elem[2]
+
+            if cur_y < radius:
+                elem[1] = radius
+                elem[3] = -elem[3]
+
+            if cur_y > size[1] - radius:
+                elem[1] = size[1] - radius
+                elem[3] = -elem[3]
         pygame.display.flip()
     pygame.quit()
