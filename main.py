ab3912abf4630915ef2188e66efb6814379a5324
AidarusIkarus <muginov.aidar@gmail.com>
2025-02-28 23:59:01 +0300

    задача 8 Куб

--- main.py
+++ main.py
@@ -1,27 +1,41 @@
 import pygame
+import os
 
 
 def bricks():
-    white = (255, 255, 255)
-    screen.fill(white)
+    x = 150 - width * 0.75
+    y = 150 - width / 4
+    half_width = width / 2
 
-    for y in range(0, size[1], 17):
-        for x in range(0, size[0], 32):
-            if y % 2 == 0:
-                x -= 15
-            pygame.draw.rect(screen, pygame.Color('red'), (x, y, 30, 15))
+    color_front = pygame.Color('white')
+    color_upper = pygame.Color('white')
+    color_side = pygame.Color('white')
+    hsv = pygame.Color('white').hsva
+
+    color_front.hsva = (hue, hsv[1] + 100, hsv[2] - 25, hsv[3])
+    color_upper.hsva = (hue, hsv[1] + 100, hsv[2], hsv[3])
+    color_side.hsva = (hue, hsv[1] + 100, hsv[2] - 50, hsv[3])
+
+    pygame.draw.polygon(screen, color_front, ((x, y), (x + width, y), (x + width, y + width), (x, y + width)))
+    pygame.draw.polygon(screen, color_upper, (
+        (x + half_width, y - half_width), (x + half_width + width, y - half_width), (x + width, y), (x, y)))
+    pygame.draw.polygon(screen, color_side, (
+        (x + width, y), (x + half_width + width, y - half_width), (x + half_width + width, y + half_width),
+        (x + width, y + width)))
 
 
 if __name__ == '__main__':
     pygame.init()
-    pygame.display.set_caption('Кирпичи')
+    pygame.display.set_caption('Куб')
 
-    # try:
-    #     num = int(input())
-    # except ValueError:
-    #     raise ValueError('Неправильный формат ввода')
+    try:
+        width, hue = map(int, input().split())
+    except ValueError:
+        raise ValueError('Неправильный формат ввода')
+    if width % 4 != 0 or width > 100 or hue < 0 or hue > 360:
+        raise ValueError('Неправильный формат ввода')
 
-    size = (300, 200)
+    size = (300, 300)
     screen = pygame.display.set_mode(size)
     bricks()
     pygame.display.flip()
