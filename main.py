fd456633b030fd7d2749e8473a4e271a513a5e4a
AidarusIkarus <muginov.aidar@gmail.com>
2025-03-01 16:00:28 +0300

    pg2 5 К щелчку

--- main.py
+++ main.py
@@ -3,25 +3,39 @@ import pygame
 if __name__ == '__main__':
     pygame.init()
     pygame.font.init()
-    pygame.display.set_caption('Я слежу за тобой!')
-    size = (200, 200)
+    pygame.display.set_caption('К щелчку')
+    size = (501, 501)
 
     screen = pygame.display.set_mode(size)
     clock = pygame.time.Clock()
     running = True
-    count = 1
+    x, y = int(501 / 2), int(501 / 2)
+    dest_x, dest_y = x, y
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == pygame.ACTIVEEVENT and event.state == pygame.APPACTIVE and event.gain == 0:
-                count += 1
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                dest_x, dest_y = event.pos
 
-        out = pygame.font.Font(None, 100).render(str(count), True, 'red')
-        screen.blit(out, (100 - out.get_rect().width / 2, 100 - out.get_rect().height / 2))
-        pygame.display.update()
+        dif_x, dif_y = x - dest_x, y - dest_y
+        if x != dest_x or y != dest_y:
+            if dif_x < 0:
+                x += 1
+            elif dif_x > 0:
+                x -= 1
+            if dif_y < 0:
+                y += 1
+            elif dif_y > 0:
+                y -= 1
+        else:
+            pass
+        x, y = int(x), int(y)
+
+        pygame.draw.circle(screen, 'red', (x, y), 20)
         pygame.display.flip()
         screen.fill('black')
+        clock.tick(60)
 
     pygame.quit()
