2a195099258a7e43cf93965129ab584f186e0d43
AidarusIkarus <muginov.aidar@gmail.com>
2025-03-01 14:34:39 +0300

    pg2 3 Перетаскивание

--- main.py
+++ main.py
@@ -1,51 +1,34 @@
 import pygame
-import os
 
 if __name__ == '__main__':
     pygame.init()
-    pygame.display.set_caption('Шарики')
+    pygame.display.set_caption('Перетаскивание')
     size = (300, 300)
 
     screen = pygame.display.set_mode(size)
     clock = pygame.time.Clock()
     running = True
     boolean = False
-    radius = 10
-    circles = []
+    x, y = 0, 0
+    prev_x, prev_y, new_x, new_y = 0, 0, 0, 0
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
-                x, y = event.pos
-                if x < radius:
-                    x = radius
-                if y < radius:
-                    y = radius
-                circles.append([x, y, 1, -1])
+                if x < event.pos[0] < x + 100 and y < event.pos[1] < y + 100:
+                    boolean = True
 
+            if event.type == pygame.MOUSEMOTION:
+                if boolean:
+                    new_x, new_y = event.rel
+                    x += new_x
+                    y += new_y
+            if event.type == pygame.MOUSEBUTTONUP:
+                boolean = False
         screen.fill('black')
-        dt = clock.tick() / 1000
-        for elem in circles:
-            cur_x, cur_y, x_dir, y_dir = elem
-            pygame.draw.circle(screen, 'white', (int(cur_x), int(cur_y)), radius)
-            elem[0] -= x_dir * 100 / 1.414 * dt
-            elem[1] += y_dir * 100 / 1.414 * dt
-            if cur_x < radius:
-                elem[0] = radius
-                elem[2] = -elem[2]
 
-            if cur_x > size[0] - radius:
-                elem[0] = size[0] - radius
-                elem[2] = -elem[2]
-
-            if cur_y < radius:
-                elem[1] = radius
-                elem[3] = -elem[3]
-
-            if cur_y > size[1] - radius:
-                elem[1] = size[1] - radius
-                elem[3] = -elem[3]
+        pygame.draw.rect(screen, 'green', (x, y, 100, 100))
         pygame.display.flip()
     pygame.quit()
